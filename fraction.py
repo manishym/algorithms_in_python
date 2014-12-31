32c32747ab95314f8822c89dc0ae1e9fa1b759aa
Manish M Yathnalli <manish.ym@gmail.com>
2014-12-31 12:30:07 +0530

    Added fractions class

---
+++ fraction.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from fractions import gcd
+
+class Fraction:
+    def __init__(self, top, bottom):
+        self.num = top/gcd(top, bottom)
+        self.den = bottom/gcd(top, bottom)
+
+    def __str__(self):
+        if self.num != self.den:
+            return str(self.num) + "/" + str(self.den) 
+        return str(1)
+
+
+    def __add__(self, other):
+        return Fraction(self.num * other.den + self.den * other.num, self.den * other.den)
+
+    def __sub__(self, other):
+        return Fraction(self.num * other.den - self.den * other.num, self.den * other.den)
+
+    def __eq__(self, other):
+        return self.num * other.den == self.den * other.num
+
+    def __div__(self, other):
+        return Fraction(self.num * other.den, other.num * self.den) 
+
+    def __mul__(self, other):
+        return Fraction(self.num * other.num, other.den * self.den) 
+    
+    def __gt__(self, other):
+        return self.num * other.den > self.den * other.num
+
+    def __lt__(self, other):
+        return self.num * other.den < self.den * other.num
+
