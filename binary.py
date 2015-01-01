f78b3d8e6f4229cf2e4a85a0103b856c911ff0cb
Manish M Yathnalli <manish.ym@gmail.com>
2015-01-01 23:36:05 +0530

    Converted binary_convert into any base

--- binary.py
+++ binary.py
@@ -2,11 +2,11 @@
 from stack import Stack
 
 
-def binary_convert(s):
+def binary_convert(s, base):
     p = Stack()
     while(s > 0):
-        p.push(s % 2)
-        s = s / 2
+        p.push(s % base)
+        s = s / base
 
     s = ""
     while not p.isEmpty():
@@ -18,10 +18,12 @@ def main():
     s = 1204
     s1 = 5
     s3 = 14
-    print binary_convert(s)
-    print binary_convert(s1)
-    print binary_convert(s3)
-
+    print binary_convert(s, 2)
+    print binary_convert(s1, 2)
+    print binary_convert(s3, 2)
+    print binary_convert(s, 3)
+    print binary_convert(s1, 3)
+    print binary_convert(s3, 3)
 
 if __name__ == '__main__':
     main()
