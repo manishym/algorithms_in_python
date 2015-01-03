c05ff723b51a6dfa8229d0114f1e1205ea1c44c9
Manish M Yathnalli <manish.ym@gmail.com>
2015-01-03 17:26:59 +0530

    Added queue implementation in python

---
+++ queue.py
@@ -0,0 +1,38 @@
+#/usr/local/bin/env python
+
+
+class Queue():
+    """A Python implementation of queue"""
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        self.items == []
+
+    def enqueue(self, obj):
+        self.items.insert(0, obj)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+    def __repr__(self):
+        return "Queue: " + str(self.items)
+
+
+def main():
+    q = Queue()
+    q.enqueue("dog")
+    q.enqueue("Cat")
+    q.enqueue(100)
+    print q
+    q.dequeue()
+    q.dequeue()
+    print q
+
+
+
+if __name__ == '__main__':
+    main()
